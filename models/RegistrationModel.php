38bce19c8b305ca7e118edbcb64ef1dc026c8186
Minhmhd4 <trungcoffeehnmos@gmail.com>
2026-05-10 10:51:55 +0700

    thêm model registration

---
+++ models/RegistrationModel.php
@@ -0,0 +1,40 @@
+<?php
+// models/RegistrationModel.php
+
+require_once __DIR__ . '/BaseModel.php';
+
+class RegistrationModel extends BaseModel {
+    protected $collection = 'registrations';
+
+    public function createRegistration($userId, $scheduleId, $status = 'pending') {
+        $registration = [
+            'user_id' => $userId,
+            'schedule_id' => $scheduleId,
+            'registration_time' => new MongoDB\BSON\UTCDateTime(),
+            'status' => $status
+        ];
+        return $this->insertOne($registration);
+    }
+
+    public function getRegistrationsByUser($userId) {
+        return $this->find(['user_id' => $userId]);
+    }
+
+    public function getRegistrationsBySchedule($scheduleId) {
+        return $this->find(['schedule_id' => $scheduleId]);
+    }
+
+    public function updateRegistrationStatus($registrationId, $status) {
+        return $this->updateOne(
+            ['_id' => new MongoDB\BSON\ObjectId($registrationId)],
+            ['$set' => ['status' => $status]]
+        );
+    }
+
+    public function isUserRegistered($userId, $scheduleId) {
+        return $this->findOne([
+            'user_id' => $userId,
+            'schedule_id' => $scheduleId
+        ]) !== null;
+    }
+}
